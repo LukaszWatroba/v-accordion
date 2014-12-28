156921a91547f652a4afa1afb2dcbd10a09708b5
Łukasz Wątroba <lukasz.jan.watroba@gmail.com>
2014-12-28 19:43:15 +0100

    v0.0.2

--- dist/v-accordion.css
+++ dist/v-accordion.css
@@ -1,6 +1,6 @@
 /**
- * vAccordion - AngularJS multi-level accordion directive.
- * @version v0.0.1
+ * vAccordion - AngularJS multi-level accordion component
+ * @version v0.0.2
  * @link http://lukaszwatroba.github.io/v-accordion
  * @author Łukasz Wątroba <l@lukaszwatroba.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
