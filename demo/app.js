04a75c1f6e748c78a549bc883423441fbd828f92
Łukasz Wątroba <lukasz.jan.watroba@gmail.com>
2014-12-28 16:02:30 +0100

    initial commit

---
+++ demo/app.js
@@ -0,0 +1,40 @@
+(function (angular) {
+  'use strict';
+
+  angular.module('myApp',
+      [
+        'vAccordion'
+      ]
+    )
+
+    .controller('AccordionController', function ($scope) {
+
+      $scope.panes = [
+        {
+          header: 'Pane 1',
+          content: 'Curabitur et ligula. Ut molestie a, ultricies porta urna. Vestibulum commodo volutpat a, convallis ac, laoreet enim. Phasellus fermentum in, dolor. Pellentesque facilisis. Nulla imperdiet sit amet magna. Vestibulum dapibus, mauris nec malesuada fames ac turpis velit, rhoncus eu, luctus et interdum adipiscing wisi.'
+        },
+        {
+          header: 'Pane 2',
+          content: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies.'
+        },
+        {
+          header: 'Pane 3',
+          content: 'Aliquam erat ac ipsum. Integer aliquam purus. Quisque lorem tortor fringilla sed, vestibulum id, eleifend justo vel bibendum sapien massa ac turpis faucibus orci luctus non.',
+
+          subpanes: [
+            {
+              header: 'Subpane 1',
+              content: 'Lorem ipsum dolor sit amet enim.'
+            },
+            {
+              header: 'Subpane 2',
+              content: 'Curabitur et ligula. Ut molestie a, ultricies porta urna. Quisque lorem tortor fringilla sed, vestibulum id.'
+            }
+          ]
+        }
+      ];
+      
+    });
+
+})(angular);
---
+++ src/vAccordion/vAccordion.js
@@ -0,0 +1,315 @@
+'use strict';
+
+// Module
+angular
+  .module('vAccordion', 
+    [
+      'ngAnimate',
+
+      'vAccordion.config',
+      'vAccordion.controllers',
+      'vAccordion.directives'
+    ]);
+
+
+// Config
+angular
+  .module('vAccordion.config', [])
+
+  .constant('accordionConfig', {
+    classes: {
+      accordion: 'Accordion Accordion--dafault',
+      pane: 'Accordion-pane',
+      paneHeader: 'Accordion-paneHeader',
+      paneContent: 'Accordion-paneContent',
+
+      expandedState: 'is-expanded'
+    }
+  });
+
+
+// Controllers
+angular
+  .module('vAccordion.controllers', [])
+
+  .controller('vAccordionController', vAccordionController)
+  .controller('vPaneController', vPaneController);
+
+function vAccordionController ($scope) {
+  var ctrl = this;
+  
+  $scope.isDisabled = false;
+  $scope.panes = [];
+
+  var hasExpandedPane = function () {
+    var bool = false;
+
+    for (var i = 0, length = $scope.panes.length; i < length; i++) {
+      var iteratedPane = $scope.panes[i];
+
+      if (iteratedPane.isExpanded) {
+        bool = true;
+        break;
+      }
+    }
+
+    return bool;
+  };
+
+  ctrl.addPane = function (pane) {
+    if (!$scope.allowMultiple && pane.isExpanded) {
+      if (hasExpandedPane()) {
+        throw new Error('allow-multiple attribute is not set');
+      } 
+    }
+
+    $scope.panes.push(pane);
+  };
+
+  ctrl.disable = function () {
+    $scope.isDisabled = true;
+  };
+
+  ctrl.enable = function () {
+    $scope.isDisabled = false;
+  };
+
+  ctrl.expandAll = function () {
+    if ($scope.isDisabled) { return; }
+
+    if ($scope.allowMultiple) {
+      angular.forEach($scope.panes, function (iteratedPane) {
+        iteratedPane.isExpanded = true;
+      });
+    } else {
+      throw new Error('allow-multiple attribute is not set');
+    }
+  };
+
+  ctrl.collapseAll = function (exceptionalPane) {
+    if ($scope.isDisabled) { return; }
+
+    angular.forEach($scope.panes, function (iteratedPane) {
+      if (iteratedPane !== exceptionalPane) {
+        iteratedPane.isExpanded = false;
+      }
+    });
+  };
+
+  ctrl.toggle = function (paneToExpand) {
+    if ($scope.isDisabled) { return; }
+
+    if ($scope.allowMultiple) {
+      paneToExpand.isExpanded = !paneToExpand.isExpanded;
+    } else {
+      ctrl.collapseAll(paneToExpand);
+      paneToExpand.isExpanded = !paneToExpand.isExpanded;
+    }
+  };
+
+  $scope.internalControl = {
+    toggle: function (paneIndex) {
+      var paneToExpand = $scope.panes[paneIndex];
+
+      if (paneToExpand) {
+        ctrl.toggle(paneToExpand);
+      }
+    },
+    expandAll: ctrl.expandAll,
+    collapseAll: ctrl.collapseAll
+  };
+}
+vAccordionController.$inject = ['$scope'];
+
+
+function vPaneController ($scope) {
+  var ctrl = this;
+
+  ctrl.toggle = function () {
+    if (!$scope.isAnimating) {
+      $scope.accordionCtrl.toggle($scope);
+    }
+  };
+}
+vPaneController.$inject = ['$scope'];
+
+
+// Directives
+angular
+  .module('vAccordion.directives', [])
+
+  .directive('vAccordion', vAccordionDirective)
+  .directive('vPane', vPaneDirective)
+  .directive('vPaneHeader', vPaneHeaderDirective)
+  .directive('vPaneContent', vPaneContentDirective);
+
+function vAccordionDirective (accordionConfig) {
+  return {
+    restrict: 'EA',
+    replace: true,
+    transclude: true,
+    template: '<div ng-transclude></div>',
+    scope: {
+      control: '=?',
+      allowMultiple: '=?'
+    },
+    controllerAs: 'accordionCtrl',
+    controller: 'vAccordionController',
+    compile: function (tElement) {
+      tElement.addClass(accordionConfig.classes.accordion);
+
+      return function postLink (scope, iElement, iAttrs) {
+        if (!angular.isDefined(scope.allowMultiple)) {
+          scope.allowMultiple = angular.isDefined(iAttrs.allowMultiple);
+        }
+
+        scope.control = scope.internalControl;
+      };
+    }
+  };
+}
+vAccordionDirective.$inject = ['accordionConfig'];
+
+
+function vPaneDirective ($timeout, $animate, accordionConfig) {
+  return {
+    restrict: 'EA',
+    require: '^vAccordion',
+    transclude: true,
+    replace: true,
+    template: '<div ng-transclude></div>',
+    scope: {
+      isExpanded: '=?expanded'
+    },
+    controllerAs: 'paneCtrl',
+    controller: 'vPaneController',
+    compile: function (tElement) {
+      tElement.addClass(accordionConfig.classes.pane);
+
+      return function postLink (scope, iElement, iAttrs, accordionCtrl) {
+        if (!angular.isDefined(scope.isExpanded)) {
+          scope.isExpanded = angular.isDefined(iAttrs.expanded);
+        }
+
+        var paneHeaderNative = iElement[0].querySelector('.' + accordionConfig.classes.paneHeader),
+            paneContentNative = iElement[0].querySelector('.' + accordionConfig.classes.paneContent);
+
+        if (!paneHeaderNative) {
+          throw new Error('v-pane-header not found');
+        }
+
+        if (!paneContentNative) {
+          throw new Error('v-pane-content not found');
+        }
+
+        var paneInnerNative = paneContentNative.querySelector('div');
+
+        var paneHeaderElement = angular.element(paneHeaderNative),
+            paneContentElement = angular.element(paneContentNative);
+
+        accordionCtrl.addPane(scope);
+        scope.accordionCtrl = accordionCtrl;
+
+        paneContentNative.style.maxHeight = '0px';
+
+        function expand () {
+          accordionCtrl.disable();
+
+          var paneInnerHeight = paneInnerNative.offsetHeight;
+          paneContentNative.style.maxHeight = '0px';
+
+          $animate.addClass(paneContentElement, accordionConfig.classes.expandedState)
+            .then(function () {
+              accordionCtrl.enable();
+              paneContentNative.style.maxHeight = 'none';
+            });
+
+          setTimeout(function () {
+            paneContentNative.style.maxHeight = paneInnerHeight + 'px';
+          }, 0);
+
+          iElement.addClass(accordionConfig.classes.expandedState);
+          paneHeaderElement.addClass(accordionConfig.classes.expandedState);
+        }
+
+        function collapse () {
+          accordionCtrl.disable();
+
+          var paneInnerHeight = paneInnerNative.offsetHeight;
+
+          paneContentNative.style.maxHeight = paneInnerHeight + 'px';
+
+          $animate.removeClass(paneContentElement, accordionConfig.classes.expandedState)
+            .then(function () {
+              accordionCtrl.enable();
+            });
+
+          setTimeout(function () {
+            paneContentNative.style.maxHeight = '0px';
+          }, 0);
+
+          iElement.removeClass(accordionConfig.classes.expandedState);
+          paneHeaderElement.removeClass(accordionConfig.classes.expandedState);
+        }
+
+        $timeout(function () {
+          if (scope.isExpanded) {
+            expand();
+          }
+        }, 100);
+
+        scope.$watch('isExpanded', function (newValue, oldValue) {
+          if (newValue === oldValue) { return true; }
+
+          if (newValue) {
+            expand();
+          } else {
+            collapse();
+          }            
+        });
+
+      };
+    }
+  };
+}
+vPaneDirective.$inject = ['$timeout', '$animate', 'accordionConfig'];
+
+
+function vPaneHeaderDirective (accordionConfig) {
+  return {
+    restrict: 'EA',
+    require: '^vPane',
+    transclude: true,
+    replace: true,
+    template: '<div ng-click="paneCtrl.toggle()"><div ng-transclude></div></div>',
+    scope: {},
+    compile: function (tElement) {
+      tElement.addClass(accordionConfig.classes.paneHeader);
+
+      return function postLink (scope, iElement, iAttrs, paneCtrl) {
+        scope.paneCtrl = paneCtrl;
+      };
+    }
+  };
+}
+vPaneHeaderDirective.$inject = ['accordionConfig'];
+
+
+function vPaneContentDirective (accordionConfig) {
+  return {
+    restrict: 'EA',
+    require: '^vPane',
+    transclude: true,
+    replace: true,
+    template: '<div><div ng-transclude></div></div>',
+    scope: {},
+    compile: function (tElement) {
+      tElement.addClass(accordionConfig.classes.paneContent);
+
+      return function postLink (scope, iElement, iAttrs, paneCtrl) {
+        scope.paneCtrl = paneCtrl;
+      };
+    }
+  };
+}
+vPaneContentDirective.$inject = ['accordionConfig'];
