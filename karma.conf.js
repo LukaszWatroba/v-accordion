97d1c771a11fc910fd817d96fbae9f67b7bf1ef4
Łukasz Wątroba <lukasz.jan.watroba@gmail.com>
2016-02-20 12:21:28 +0100

    switch to Chrome launcher

--- karma.conf.js
+++ karma.conf.js
@@ -66,7 +66,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
